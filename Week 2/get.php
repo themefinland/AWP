c1e47ba9e1ff8f889df40c9914b03f04e1e0ff2a
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2015-01-17 16:13:11 +0200

    week 2 updated

--- Week 2/get.php
+++ Week 2/get.php
@@ -1,4 +1,4 @@
-<div id="accordion">
+<div id="calendar">
 <?php
     $host = 'mysql.metropolia.fi';
     $dbname = ''; // your username
@@ -11,23 +11,40 @@
             $DBH->query("SET NAMES 'UTF8';");
         
         } catch(PDOException $e) {
-            echo "Something went wrong.";
+            echo "Could not connect to database.";
             file_put_contents('log.txt', $e->getMessage(), FILE_APPEND);
         }
 
-
+try {
     $eventList = array();
-    $sql = "SELECT * FROM kalenteri";
-    $STH = @$DBH->query($sql);
+    $sql = "SELECT * FROM calendar ORDER BY eDate ASC";
+    $STH = $DBH->query($sql);
     $STH->setFetchMode(PDO::FETCH_OBJ);
     while ($row = $STH->fetch()){
-    	echo '<h3>'.$row->pvm.'<br><strong>'.$row->nimi.'</strong></h3><div><i>'.$row->kuvaus.'</i><br><br>With:<br>'.$row->email.'<br>'.$row->puh.'</div>';
+		// create standard object which complies with this: http://fullcalendar.io/docs/event_data/Event_Object/
+		$event = new stdClass();
+    	$event->start = $row->eDate;
+		// TODO: you also need
+		// title
+		// description
+		// email
+		// phone
+		$eventList[] = $event;
     }
-       
+	$eventsJSON = json_encode($eventList);
+ } catch (PDOException $e) {
+	echo 'Something went wrong';
+	file_put_contents('log.txt', $e->getMessage()."\n\r", FILE_APPEND); // remember to set the permissions so that log.txt can be created
+}      
 
 ?>
 </div>
 <script>
-// use jQuery UI to make an accordion from <div id="accordion">. 
-// Set 'collapsible' property to 'true' and 'active' property to 'none'
+// JSON made in PHP is saved in JavaScript
+var jsonEvents = <?php echo $eventsJSON; ?>;
+console.log(jsonEvents);
+// TODO: use jQuery FullCalendar plugin to display the events in a proper calendar
+// calendar has to have at least month, week and day views
+// timeformat is 24-hour clock
+// when an event is clicked all the event details (jsonEvents) are displayed	
 </script>
--- Week 2/save.php
+++ Week 2/save.php
@@ -5,8 +5,17 @@
     $pass = ''; // your database password
 	
    // TODO: get the data from the form by using $_POST
+   $data = array();
+   $data['name'] = $_POST['name'];
+   $data['description'] = $_POST['desc'];
+   $data['email'] = $_POST['email'];
+   $data['phone'] = $_POST['cell'];
+   $date = $_POST['date'];
+   $time = $_POST['time'];
    // this is how you convert the date from the form to SQL formatted date:
    // date ("Y-m-d H:i:s", strtotime(dataFromDateField.' '.dataFromTimeField));
+   $data['sqlDate'] = date ("Y-m-d H:i:s", strtotime($date.' '.$time));;
+   
    
 // this part was in dbConnect.php in last period:
 try {
@@ -23,10 +32,10 @@ try {
 
     
 try {
-
-
 	// TODO: insert the data from the form to database table 'calendar'
-	
+	$STH = $DBH->prepare("INSERT INTO calendar (eName, eDescription, pEmail, pPhone, eDate) VALUES
+	(:name, :description, :email, :phone, :sqlDate);");
+	$STH->execute($data);
 
 } catch (PDOException $e) {
 	echo 'Something went wrong';
